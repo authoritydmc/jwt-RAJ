5a3ba2018a65a0573677a994fb0e4edf19b1666b
beast <com.mailuser@gmail.com>
2023-05-13 23:39:36 +0530

    Initial commit

---
+++ src/main/java/com/rajdubey/jwtRAJ/JwtRajApplication.java
@@ -0,0 +1,13 @@
+package com.rajdubey.jwtRAJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtRajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtRajApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rajdubey/jwtRAJ/JwtRajApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajdubey.jwtRAJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtRajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
